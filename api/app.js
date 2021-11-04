1f628bb43c5108abd67a26960fce8afe0bc850b4
Vikasitha-Lab <vikasithaex@gmail.com>
2021-11-04 18:53:54 +0530

    setup body-parser package

--- api/app.js
+++ api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = 5000;
+
+//Setup middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.json('api is working!');
@@ -12,4 +19,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`API is listening on PORT: ${PORT}`);
 });
-
