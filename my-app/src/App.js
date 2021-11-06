054c8fb19121849f3fa75e4527bb9fdb0bd6392d
Vikasitha-Lab <vikasithaex@gmail.com>
2021-11-06 18:57:31 +0530

    init form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
 
 function App() {
   return (
     <div className="App">
-      <p>multi tier docker my-app</p>
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter full name" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>E-mail Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter e-mail address" />
+                <Form.Text className="text-muted">
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              
+              <Stack gap={2} className="col-md-5 mx-auto">
+                <Button variant="primary">SUBMIT</Button>
+                <Button variant="outline-secondary">CANCEL</Button>
+              </Stack>
+
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
